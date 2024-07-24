2bf6e9710cb0b6d4c34b7cb086e2f2d30292a2f4
Toma Alexandru <alextoma73.at@gmail.com>
2024-07-24 17:18:14 +0300

    fix ex 1

--- src/org/beta/curs12/homework/ex1/Main.java
+++ src/org/beta/curs12/homework/ex1/Main.java
@@ -6,7 +6,7 @@ import org.beta.curs12.homework.ex1.exceptions.InvalidAgeException;
 public class Main {
     public static void main(String[] args) throws InvalidAgeException {
 
-        BirthYearCalculator calculator = new BirthYearCalculator(null);
+        BirthYearCalculator calculator = new BirthYearCalculator(21, false);
         int birthYear = calculator.calculateBirthYear();
         System.out.println("Your birth year is: " + birthYear);
 
--- src/org/beta/curs12/homework/ex1/calculators/BirthYearCalculator.java
+++ src/org/beta/curs12/homework/ex1/calculators/BirthYearCalculator.java
@@ -1,22 +1,28 @@
 package org.beta.curs12.homework.ex1.calculators;
 
 import org.beta.curs12.homework.ex1.exceptions.InvalidAgeException;
-
 import java.time.Year;
 
 public class BirthYearCalculator {
-    private final Integer age;
+    private final int age;
+    private final boolean birthdayAlreadyPassed;
 
-    public BirthYearCalculator(Integer age) throws InvalidAgeException {
+    public BirthYearCalculator(int age, boolean birthdayAlreadyPassed) throws InvalidAgeException {
         if (age <= 0) {
             throw new InvalidAgeException("Age must be a positive integer.");
         }
         this.age = age;
+        this.birthdayAlreadyPassed = birthdayAlreadyPassed;
     }
 
-    public Integer calculateBirthYear() {
+    public int calculateBirthYear() {
         Year currentYear = Year.now();
         int currentYearValue = currentYear.getValue();
-        return currentYearValue - age;
+
+        if (birthdayAlreadyPassed) {
+            return currentYearValue - age;
+        } else {
+            return currentYearValue - age - 1;
+        }
     }
 }
